eddb9773cac02b747500d2be11bbd00d581abc14
Bruno Pavan Ciriani da Silva <brunopavanciriani@gmail.com>
2024-01-09 22:59:12 -0300

    Bigger Maze, Less Delay

--- maze.py
+++ maze.py
@@ -1,11 +1,11 @@
 from pyamaze import maze, agent
 from A_star_search import a_star_search
 
-my_maze = maze()
+my_maze = maze(50, 50)
 my_maze.CreateMaze()
 
 my_agent = agent(my_maze, filled=True, footprints=True)
 path     = a_star_search(my_maze)
 
-my_maze.tracePath({my_agent : path}, delay=300)
+my_maze.tracePath({my_agent : path}, delay=20)
 my_maze.run()
